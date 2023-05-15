d64f6d98708396faa3981d7d23e2c4d794b30a9f
oliveirarod <rodrigo.oliveira6446@gmail.com>
2023-05-15 19:58:26 -0300

    Cria a estrutura do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClientsComponent } from './pages/clients/clients.component';
+import { ClientCreateComponent } from './pages/client-create/client-create.component';
+import { ClientEditComponent } from './pages/client-edit/client-edit.component';
+
+export const ROUTE_PATHS = {
+  CLIENTS: 'clients',
+  CLIENT_CREATE: 'client-create',
+  CLIENT_EDIT: 'client-edit',
+};
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: ROUTE_PATHS.CLIENTS,
+    pathMatch: 'full',
+  },
+  {
+    path: ROUTE_PATHS.CLIENTS,
+    component: ClientsComponent,
+  },
+  {
+    path: ROUTE_PATHS.CLIENT_CREATE,
+    component: ClientCreateComponent,
+  },
+  {
+    path: `${ROUTE_PATHS.CLIENT_EDIT}/:id`,
+    component: ClientEditComponent,
+  },
+  {
+    path: '**',
+    redirectTo: ROUTE_PATHS.CLIENTS,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/client-create/client-create.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientCreateComponent } from './client-create.component';
+
+
+@NgModule({
+  declarations: [
+    ClientCreateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClientCreateModule { }
